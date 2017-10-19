f5311cbbdb76aac122e69ac27571ce9fb40b2e62
yanjq <yanjq@realbull.net>
2017-10-19 11:06:20 +0800

    no message

---
+++ test/constants.js
@@ -0,0 +1,6 @@
+var constants = {
+	TEST_ON:1,
+	TEST_OFF:2
+}
+
+module.exports = constants;
--- test/shmj.js
+++ test/shmj.js
@@ -1,45 +1,59 @@
-require('babel-register')
-var http = require('http')
-var test = require('tape')
-var hostname = "shmj.local.com"
-var token = "ghSgB0V6AyJSG8j8nOq4BKp2qHo6b0fPxrLU1R9yend73Ibv2vyOl75HjB0DJe4dUlEsTWBqPhV4Ga5MJUYZ-S5Bi.6EhRq0oQJ7eDQuboCtb2J0VzmD3A!!"
-function getApiData(url, params, cb) {
-    var options = {
-        hostname: hostname,
-        port: 80,
-        method: 'GET'
-    };
-    var path = url+"?token="+token
-    for (var i in params) {
-        path += ("&"+params[i].key+"="+params[i].val)
-    }
-    options.path = path;
-    //发送请求
-    var req = http.request(options,function(res){
-        res.setEncoding('utf8');
-        res.on('data',function(chunk){
-            returnData = JSON.parse(chunk);//如果服务器传来的是json字符串，可以将字符串转换成json
-            cb(returnData)
-        });
-    });
-    //如果有错误会输出错误
-    req.on('error', function(e){
-         console.log('错误：' + e.message);
-    });
-    req.end();
-}
+const baselib = require('./base');
+const constants = require('./constants');
+const hostname = "shmj.local.com";
+const port = 80;
+const token = "ghSgB0V6AyJSG8j8nOq4BKp2qHo6b0fPxrLU1R9yend73Ibv2vyOl75HjB0DJe4dUlEsTWBqPhV4Ga5MJUYZ-S5Bi.6EhRq0oQJ7eDQuboCtb2J0VzmD3A!!";
 
-test("newerlevelup test", t =>{
-   
-    getApiData("/mobile/newerLevelUp", [], function(res){
+
+var base = new baselib(hostname, port, token);
+
+base.funytest(constants.TEST_OFF, "newerlevelup test", function(t){
+   base.getApiData("/mobile/newerLevelUp", [], function(res){
         data = res.data
         t.equal(res.ret, 0)
         t.equal(data.userLevel, 20)
         t.end()
     })
     
-    getApiData("/mobile/newerLevelUpAwardGet", [{key:"level", val:21}], function(res){
+    base.getApiData("/mobile/newerLevelUpAwardGet", [{key:"level", val:21}], function(res){
         t.equal(res.ret, 1)
         t.equal(res.msg, "该等级奖励不存在")
     })
+});
+
+
+base.funytest(constants.TEST_ON, "weixinPay gift", function(t){
+     var post_data = {
+        return_code:"SUCCESS", 
+        result_code:"SUCCESS", 
+        transaction_id:"123", 
+        out_trade_no:"1"
+    };
+    base.postApiData("/mobile/weixinCallback/6/101", [], post_data, function(res){
+        t.pass("the result is callback:");
+        console.log(res);
+        t.end();
+    })
+});
+
+
+base.funytest(constants.TEST_OFF, "applePay gift", function(t){
+    var post_data = {
+
+    }
+    base.postApiData("/mobile/appstoreCallback",[], post_data, function(res){
+      t.pass("the result is callback:");
+        console.log(res);
+        t.end();  
+    });
+});
+
+
+base.funytest(constants.TEST_OFF, "exchange gift", function(t){
+    var data = [{key:"gift_id", val:"150"}, {key:"name", val:"小燕"}, {key:"tel", val:"18516536416"}, {key:"address", val:"上海市浦东新区123号"} ];
+   base.getApiData("/mobile/ticketOrder", data, function(res){
+        t.pass("the result is callback");
+        console.log(res);
+        t.end();
+    });
 });
